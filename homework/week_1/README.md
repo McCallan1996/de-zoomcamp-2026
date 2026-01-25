fe8a9d06d881c733fc4579df633f85c4c899b079
McCallan1996 <khuankhun.n1@gmail.com>
2026-01-26 01:31:03 +0700

    Q3 answered

--- homework/week_1/README.md
+++ homework/week_1/README.md
@@ -1,11 +1,27 @@
 # Homework Week 1: Docker & SQL
 
 ## Question 1. Understanding Docker images
+
 **Command:** `docker run -it --entrypoint bash python:3.13`
+
 **Answer:** 25.3
 
 ## Question 2. Understanding Docker networking and docker-compose
+
 **Answer:** db:5432 (or postgres:5432)
+
 **Reasoning:**
 - **Hostname:** Within the Docker network, containers communicate using their service name (`db`) or container name (`postgres`).
 - **Port** They communicate internally via the container's internal port (`5432`).
+
+## Question 3. Counting short trips
+
+**SQL Query:**
+```sql
+SELECT count(*) 
+FROM green_taxi_trips 
+WHERE lpep_pickup_datetime >= '2025-11-01' 
+AND lpep_pickup_datetime < '2025-12-01' 
+AND trip_distance <= 1;
+```
+**Answer:** 8,007
---
+++ homework/week_1/ingest_data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+# user: postgres, password: postgres, port: 5433 (docker-compose), db: ny_taxi
+db_url = 'postgresql://postgres:postgres@postgres:5432/ny_taxi'
+engine = create_engine(db_url)
+
+def upload_to_postgres(file_path, table_name, file_type='parquet'):
+    print(f"Loarding from {file_path} to {table_name}...")
+
+    if file_type == 'parquet':
+        df = pd.read_parquet(file_path)
+    else:
+        # CSV
+        df = pd.read_csv(file_path)
+
+    # To Database
+    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+
+    print(f"Loarded Qty: {len(df)}")
+
+if __name__ == '__main__':
+    # 1. Green Taxi
+    upload_to_postgres(
+        file_path='https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet', 
+        table_name='green_taxi_trips', 
+        file_type='parquet'
+    )
+
+    # 2. Zones
+    upload_to_postgres(
+        file_path='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv', 
+        table_name='zones', 
+        file_type='csv'
+    )
+
+
+# In[ ]:
+
+
+
+
